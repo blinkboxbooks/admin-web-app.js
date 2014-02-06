86e1ba60f519ac5169a857d858bfb641d35573e0
Vilmos Ioo <vilmosi@blinkbox.com>
2014-02-06 16:47:46 +0000

    Cleaned up login form directive

--- app/scripts/forms/login.js
+++ app/scripts/forms/login.js
@@ -7,7 +7,18 @@ angular.module('adminPanelApp')
 			templateUrl: 'views/templates/login_form.html',
 			scope: {},
 			replace: true,
-			controller: function(){},
+			controller: function($scope){
+				$scope.login = {
+					email: '',
+					password: '',
+					remember: false
+				}
+				$scope.handlers = {
+					submit: function(){
+						console.log('SUBMIT:', $scope.login);
+					}
+				}
+			},
 			link: function(){}
 		};
 	});
