11c7cd0c29c4ba4bea778888d16454bb1b5a5287
Vilmos Ioo <vilmosi@blinkbox.com>
2014-02-14 17:14:48 +0000

    Hiding pagination when displaying only one page

--- app/scripts/directives/dynamic_table.js
+++ app/scripts/directives/dynamic_table.js
@@ -13,16 +13,29 @@ angular.module('adminPanelApp')
 					'last_name': 'BBB',
 					'username': 'username'
 				}];
-				$scope.settings = {
-					// number of rows to display per page
-					'iDisplayLength': 15,
-					// controlling the generated table html
-					'sDom': '<\'row-fluid1\'<\'span6\'lp>\r>t<\'row-fluid\'<\'span6\'i>>'
-				};
+
+				$timeout(function(){
+					for(var i=0; i< 100; i++){
+						$scope.users.push({
+							'first_name': 'AAA',
+							'last_name': 'BBB',
+							'username': 'username'
+						});
+					}
+				}, 7000);
 			},
 			link: function(scope, element){
 				// Enable the datatable plugin on your directive.
-				element.dataTable(scope.settings);
+				element.dataTable({
+					// number of rows to display per page
+					'iDisplayLength': 15,
+					// controlling the generated table html
+					'sDom': '<\'row-fluid pagination\'<\'span6\'lp>\r>t<\'row-fluid pagination\'<\'span6\'i>>',
+					'fnDrawCallback': function(){
+						// add paged class to table wrapper if we have more than one page
+						element.parent().toggleClass('paged', this.fnPagingInfo().iTotalPages > 1);
+					}
+				});
 
 				// sync table with scope collection
 				scope.$watchCollection('users', function(old, value){
