693c7b491d6e5a30d2817b43fc7d53676afdb463
Vilmos Ioo <vilmosi@blinkbox.com>
2014-02-14 15:23:30 +0000

    Dynamic table populated by JS

--- app/scripts/directives/dynamic_table.js
+++ app/scripts/directives/dynamic_table.js
@@ -8,20 +8,30 @@ angular.module('adminPanelApp')
 			scope: {},
 			replace: true,
 			controller: function($scope){
-				$scope.labels = ['First Name', 'Last Name', 'Email'];
 				$scope.users = [{
-					'first_name': 'first_name',
-					'last_name': 'last_name',
+					'first_name': 'AAA',
+					'last_name': 'BBB',
 					'username': 'username'
 				}];
-				$timeout(function(){},2000);
+				$scope.settings = {
+					'iDisplayLength': 15 // number of rows to display per page
+				};
 			},
 			link: function(scope, element){
 				// Enable the datatable plugin on your directive.
-				element.DataTable();
-				scope.$watchCollection('users', function(){
+				element.DataTable(scope.settings);
+
+				// sync table with scope collection
+				scope.$watchCollection('users', function(old, value){
 					element.fnClearTable();
-					element.fnAddData([]);
+
+					var users = [];
+					for(var i = 0, l = value.length; i < l; i++){
+						var user = value[i];
+						users.push([user.first_name, user.last_name, user.username]);
+					}
+
+					element.fnAddData(users);
 				});
 
 			}
