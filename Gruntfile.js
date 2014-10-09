7684b6359d4b10c4ad04cab3e7c51adf750e4f11
Sebastian Tschan <sebastiant@blinkbox.com>
2014-10-09 17:56:49 +0100

    Update generated constants file on JSON file updates.

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,6 +29,10 @@ module.exports = function (grunt) {
 
     // Watches files for changes and runs tasks based on the changed files
     watch: {
+	    constants: {
+		    files: ['<%= yeoman.app %>/scripts/config/*.json'],
+		    tasks: ['ngconstant']
+	    },
       js: {
         files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
         tasks: ['newer:jshint:all'],
