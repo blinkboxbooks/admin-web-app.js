e54e09c5c62ea6e518638b99eca5c5a5ae88aa4e
mohamede <mohamede@blinkbox.com>
2014-12-03 17:46:43 +0000

    Add campaign request to get campaign details

--- app/scripts/controllers/campaign.js
+++ app/scripts/controllers/campaign.js
@@ -3,6 +3,11 @@
 /**
  * The Campaign controller shows information about a particular campaign
  **/
-angular.module('adminPanelApp').controller('CampaignCtrl', function ($routeParams, $scope) {
-  $scope.campaignId = $routeParams.id;
+angular.module('adminPanelApp').controller('CampaignCtrl', function ($routeParams, $scope, Campaign) {
+  $scope.campaignId = +$routeParams.id;
+  if($scope.campaignId >= 0){
+    Campaign.get($scope.campaignId).then(function(campaignDetails){
+      $scope.campaignDetails = campaignDetails;
+    });
+  }
 });
