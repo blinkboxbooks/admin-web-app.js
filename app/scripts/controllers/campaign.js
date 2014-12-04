41d045b2f7f590373864b3692a0b461da283531b
mohamede <mohamede@blinkbox.com>
2014-12-04 15:18:52 +0000

    Better layout for campaign details

--- app/scripts/controllers/campaign.js
+++ app/scripts/controllers/campaign.js
@@ -11,6 +11,18 @@ angular.module('adminPanelApp').controller('CampaignCtrl', function ($routeParam
 
     Campaign.get($scope.campaignId).then(function(campaignDetails){
       $scope.campaignLoading = false;
+      if(campaignDetails.endDate){
+        var start = new Date(campaignDetails.startDate);
+        var end = new Date(campaignDetails.endDate);
+        var now = new Date();
+        var totalDuration = end - start;
+        var nowDuration = now - start;
+        campaignDetails.daysRemaining = Math.floor((totalDuration - nowDuration) / 86400000);
+      }
+
+      campaignDetails.redemptionPercentage = Math.floor(campaignDetails.totalRedeemedVoucherCount / campaignDetails.totalVoucherCount * 100);
+      campaignDetails.redemptionPercentage = campaignDetails.redemptionPercentage < 100 ? campaignDetails.redemptionPercentage : 100;
+
       $scope.campaignDetails = campaignDetails;
     });
   }
